738a3e31fb484e9b13c25b376f1802d418d4c304
Starfish <daniel.cagara@googlemail.com>
2018-03-19 17:36:46 +1000

    missing file

---
+++ src/main/java/com/realitysink/cover/builtins/CoverCheckPowBuiltin.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Gerard Krol
+ * Copyright (c) 2018 Tyler Durden (GPG AAB252C6)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.realitysink.cover.builtins;
+
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import com.realitysink.cover.nodes.CoverType;
+import com.realitysink.cover.nodes.CoverTypedExpressionNode;
+
+@NodeInfo(shortName = "check_pow")
+@NodeChildren({@NodeChild("a"),@NodeChild("b"),@NodeChild("c"),@NodeChild("d")})
+public abstract class CoverCheckPowBuiltin extends CoverTypedExpressionNode {
+
+
+    @Specialization
+    public int checkpow(int a, int b, int c, int d) {
+        return 1;
+    }
+
+    @Override
+    public CoverType getType() {
+        return CoverType.SIGNED_INT;
+    }    
+}
